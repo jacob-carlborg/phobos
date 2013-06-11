6c52c2c6ed851e142a7436d75ab593c7375be22c
Jacob Carlborg <doob@me.com>
2013-06-11 15:42:54 +0200

    Take advantage of package modules.

--- std/serialization/package.d
+++ std/serialization/package.d
@@ -4,7 +4,7 @@
  * Version: Initial created: Jan 26, 2010
  * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
  */
-module orange.serialization._;
+module std.serialization;
 
 public:
 
