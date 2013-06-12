4734b6128cd51a3560eceab3e9b8c09738516b97
Jacob Carlborg <doob@me.com>
2013-06-12 09:54:37 +0200

    Add unit test modules.

---
+++ std/serialization/tests/enumconstantmember.d
@@ -0,0 +1,53 @@
+/**
+ * Copyright: Copyright (c) 2013 Jacob Carlborg. All rights reserved.
+ * Authors: Juan Manuel
+ * Version: Initial created: Apr 14, 2013
+ * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
+ */
+module std.serialization.tests.enumconstantmember;
+
+version (unittest):
+private:
+
+import std.serialization.Serializer;
+import std.serialization.archives.XmlArchive;
+import std.serialization.tests.util;
+
+Serializer serializer;
+XmlArchive!(char) archive;
+
+class G
+{
+	int a;
+	enum int someConstant = 4 * 1024;
+}
+
+G g;
+
+unittest
+{
+	archive = new XmlArchive!(char);
+	serializer = new Serializer(archive);
+
+	g = new G;
+	g.a = 123;
+
+	describe("serialize enum") in {
+		it("shouldn't fail to compile when there is a constant enum member") in {
+			serializer.reset();
+			serializer.serialize(g);
+
+			assert(archive.data().containsDefaultXmlContent());
+			assert(archive.data().containsXmlTag("object", `runtimeType="tests.EnumConstantMember.G" type="tests.EnumConstantMember.G" key="0" id="0"`));
+			assert(archive.data().containsXmlTag("int", `key="a" id="1"`, "123"));
+		};
+	};
+
+
+	describe("deserialize enum") in {
+		it("shouldn't fail to deserialize when there is a constant enum member") in {
+			auto gDeserialized = serializer.deserialize!(G)(archive.untypedData);
+			assert(g.a == gDeserialized.a);
+		};
+	};
+}
