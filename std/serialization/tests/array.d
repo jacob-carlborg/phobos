87426c52d236212bcc18a415498d5ba1adfd0825
Jacob Carlborg <doob@me.com>
2013-06-12 11:55:52 +0200

    First merged unit test now passes.

--- std/serialization/tests/array.d
+++ std/serialization/tests/array.d
@@ -13,9 +13,6 @@ import std.serialization.serializer;
 import std.serialization.archives.xmlarchive;
 import std.serialization.tests.util;
 
-Serializer serializer;
-XmlArchive!(char) archive;
-
 class D
 {
 	int[] arr;
@@ -23,35 +20,44 @@ class D
 
 D d;
 
-unittest
+XmlArchive!(char) archive;
+Serializer serializer;
+
+void beforeEach ()
 {
-	archive = new XmlArchive!(char);
+    archive = new XmlArchive!(char);
 	serializer = new Serializer(archive);
 
 	d = new D;
 	d.arr = [27, 382, 283, 3820, 32, 832].dup;
+    serializer.serialize(d);
+}
 
-	describe("serialize array") in {
-		it("should return a serialized array") in {
-			serializer.reset;
-			serializer.serialize(d);
+@desc("serialize array")
+{
+    @it("should return a serialized array") unittest
+    {
+        beforeEach();
 
-			assert(archive.data().containsDefaultXmlContent());
-			assert(archive.data().containsXmlTag("object", `runtimeType="tests.Array.D" type="tests.Array.D" key="0" id="0"`));
-			assert(archive.data().containsXmlTag("array", `type="int" length="6" key="arr" id="1"`));
-			assert(archive.data().containsXmlTag("int", `key="0" id="2"`, "27"));
-			assert(archive.data().containsXmlTag("int", `key="1" id="3"`, "382"));
-			assert(archive.data().containsXmlTag("int", `key="2" id="4"`, "283"));
-			assert(archive.data().containsXmlTag("int", `key="3" id="5"`, "3820"));
-			assert(archive.data().containsXmlTag("int", `key="4" id="6"`, "32"));
-			assert(archive.data().containsXmlTag("int", `key="5" id="7"`, "832"));
-		};
-	};
+        assert(archive.data().containsDefaultXmlContent());
+        assert(archive.data().containsXmlTag("object", `runtimeType="std.serialization.tests.array.D" type="std.serialization.tests.array.D" key="0" id="0"`));
+        assert(archive.data().containsXmlTag("array", `type="int" length="6" key="arr" id="1"`));
+        assert(archive.data().containsXmlTag("int", `key="0" id="2"`, "27"));
+        assert(archive.data().containsXmlTag("int", `key="1" id="3"`, "382"));
+        assert(archive.data().containsXmlTag("int", `key="2" id="4"`, "283"));
+        assert(archive.data().containsXmlTag("int", `key="3" id="5"`, "3820"));
+        assert(archive.data().containsXmlTag("int", `key="4" id="6"`, "32"));
+        assert(archive.data().containsXmlTag("int", `key="5" id="7"`, "832"));
+    }
+}
 
-	describe("deserialize array") in {
-		it("should return a deserialize array equal to the original array") in {
-			auto dDeserialized = serializer.deserialize!(D)(archive.untypedData);
-			assert(d.arr == dDeserialized.arr);
-		};
-	};
+@desc("deserialize array")
+{
+    @it("should return a deserialize array equal to the original array") unittest
+    {
+        beforeEach();
+
+        auto dDeserialized = serializer.deserialize!(D)(archive.untypedData);
+        assert(d.arr == dDeserialized.arr);
+    }
 }
