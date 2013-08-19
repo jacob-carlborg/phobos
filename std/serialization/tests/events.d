183ab00b0ef866e0c301fa56e491f23e1be65c09
Jacob Carlborg <doob@me.com>
2013-08-19 18:04:49 +0200

    Remove all mixin annotations.
    These are unnecessary since we have user defined attributes :)

--- std/serialization/tests/events.d
+++ std/serialization/tests/events.d
@@ -21,63 +21,29 @@ XmlArchive!(char) archive;
 int b;
 int c;
 
-int udaB;
-int udaC;
-
 class Events
 {
     int a;
     int d;
 
-    int udaA;
-    int udaD;
-
-    void serializing ()
+    @onSerializing void serializing ()
     {
         a = 3;
     }
 
-    mixin OnSerializing!(serializing);
-
-    @onSerializing void udaSerializing ()
-    {
-        udaA = 3;
-    }
-
-    void serialized ()
+    @onSerialized void serialized ()
     {
         b = 4;
     }
 
-    mixin OnSerialized!(serialized);
-
-    @onSerialized void udaSerialized ()
-    {
-        udaB = 4;
-    }
-
-    void deserializing ()
+    @onDeserializing void deserializing ()
     {
         c = 5;
     }
 
-    mixin OnDeserializing!(deserializing);
-
-    @onDeserializing void udaDeserializing ()
-    {
-        udaC = 5;
-    }
-
-    void deserialized ()
+    @onDeserialized void deserialized ()
     {
         d = 6;
-    }
-
-    mixin OnDeserialized!(deserialized);
-
-    @onDeserialized void udaDeserialized ()
-    {
-        udaD = 6;
     }
 }
 
@@ -85,7 +51,7 @@ Events events;
 
 void beforeEach ()
 {
-       archive = new XmlArchive!(char);
+    archive = new XmlArchive!(char);
     serializer = new Serializer(archive);
 
     events = new Events;
@@ -105,18 +71,13 @@ void beforeEach ()
         assert(archive.data().containsXmlTag("int", `key="a" id="1"`, "3"));
         assert(archive.data().containsXmlTag("int", `key="d" id="2"`, "0"));
 
-        assert(archive.data().containsXmlTag("int", `key="udaA" id="3"`, "3"));
-        assert(archive.data().containsXmlTag("int", `key="udaD" id="4"`, "0"));
-
-
         assert(b == 4);
-        assert(udaB == 4);
     }
 }
 
-@describe("deserialize class with a base class")
+@describe("deserialize a class with event handlers")
 {
-    @it("should return a deserialized string equal to the original string") unittest
+    @it("should return deserialized class with the correct values set by the event handlers") unittest
     {
         beforeEach();
 
@@ -125,10 +86,6 @@ void beforeEach ()
         assert(eventsDeserialized.a == 3);
         assert(eventsDeserialized.d == 6);
 
-        assert(eventsDeserialized.udaA == 3);
-        assert(eventsDeserialized.udaD == 6);
-
         assert(c == 5);
-        assert(udaC == 5);
     }
 }
